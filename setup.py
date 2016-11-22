1e4cb4fb80ce292ac9e3719daa2285fa1fded7f3
Justin Garen <justin@styleseat.com>
2016-11-21 18:17:07 -0800

    Change version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='ss_logutils',
-    version='0.1.1',
+    version='0.1.2',
     author='StyleSeat',
     description='Custom logging support',
     url='https://github.com/styleseat/ss_logutils',
